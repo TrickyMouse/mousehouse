29c82edbec810f683a0ce95aafbff44eb3d19639
Keith Greene <t@trickymouse.ca>
2018-06-08 11:45:31 -0400

    Add gating views etc

--- mousehouse/models.py
+++ mousehouse/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.db.models import permalink
+from tinymce.models import HTMLField
 
 # Create your models here.
+class Blog(models.Model):
+    PG = 1
+    ADULT = 2
+    ADULT_PLUS = 3
+    STANDARD = 4
+
+    TYPES = (
+        (PG, 'PG Post'),
+        (ADULT, 'Adult Post'),
+        (ADULT_PLUS, 'Adult Plus Post'),
+        (STANDARD, 'Standard Post')
+    )
+
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    body = HTMLField()
+    posted = models.DateField(db_index=True, auto_now_add=True)
+    category = models.ForeignKey('mousehouse.Category')
+    image_link = models.CharField(max_length=255, unique=False, null=True, blank=True)
+    album_link = models.CharField(max_length=255, unique=False, null=True, blank=True)
+    permission_type = models.SmallIntegerField(choices=TYPES, blank=True, null=True)
+
+    class Meta:
+        permissions = (
+            ("view_pg", "Can see PG Posts"),
+            ("view_adult", "Can see Adult Posts"),
+            ("view_adult_plus", "Can see Adult Plus Posts"),
+            ("view_standard", "Can see Standard Posts"),
+        )
+
+    
+    def __unicode__(self):
+        return '%s' % self.title
+
+    @permalink
+    def get_absolute_url(self):
+        return ('view_blog_post', None, { 'slug': self.slug })
+
+class Category(models.Model):
+    title = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, db_index=True)
+    
+    def __unicode__(self):
+        return '%s' % self.title
+
+    @permalink
+    def get_absolute_url(self):
+        return ('view_blog_category', None, { 'slug': self.slug })
--- mousehouse/views.py
+++ mousehouse/views.py
@@ -1,5 +1,68 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from mousehouse.models import Blog, Category
+from django.shortcuts import render_to_response, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django import forms
+from .forms import UserRegistrationForm
+from django.contrib.auth.models import User, Group
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
+from django.template.defaulttags import register
+
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
 
 def index(request):
-    return HttpResponse("Hello, world! This is our first view.")
+    return render(request, 'index.html', {
+        'categories': Category.objects.all(),
+        'posts': Blog.objects.all()[:5]
+    })
+
+@login_required
+def view_post(request, slug):
+    post_types = {
+        1: 'mousehouse.view_pg',
+        2: 'mousehouse.view_adult',
+        3: 'mousehouse.view_adult_plus',
+        4: 'mousehouse.view_standard'
+    }
+    post = get_object_or_404(Blog, slug=slug)
+    permissions = list(request.user.get_group_permissions())
+    
+    if post_types[post.permission_type] in permissions:
+        return render(request, 'view_post.html', {
+            'post': post
+        })
+    else:
+        return render(request, 'not_permitted.html')
+
+@login_required
+def view_category(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    return render(request, 'view_category.html', {
+        'category': category,
+        'posts': Blog.objects.filter(category=category)[:5]
+    })
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            userObj = form.cleaned_data
+            username = userObj['username']
+            email =  userObj['email']
+            password =  userObj['password']
+            if not (User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists()):
+                User.objects.create_user(username, email, password)
+                user = authenticate(username = username, password = password)
+                standard_group = Group.objects.get(name='standard')
+                standard_group.user_set.add(user) 
+                login(request, user)
+                return HttpResponseRedirect('/')
+            else:
+                raise forms.ValidationError('Looks like a username with that email or password already exists')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'registration/register.html', {'form' : form})
